3d37738fd37b51a6b7f496d9187ba0e7350ecd94
comehope <comehope@163.com>
2018-09-26 12:16:10 +0800

    use gsap animation instead of css animation

--- 144-pattern-animation/README.md
+++ 144-pattern-animation/README.md
@@ -4,4 +4,4 @@ See demo on Codepen: https://codepen.io/comehope/pen/gdVObN
 
 Watch interactive video on Scrimba: https://scrimba.com/p/pEgDAM/caRLack
 
-Read annotated source on Segmentfault(zh-cn): 
+Read annotated source on Segmentfault(zh-cn): https://segmentfault.com/a/1190000016521212
--- 144-pattern-animation/script.js
+++ 144-pattern-animation/script.js
@@ -1,4 +1,4 @@
-const SIDE_LENGTH = 5;
+const SIDE_LENGTH = 10;
 
 let container = d3.select('.container')
     .style('--side-length', SIDE_LENGTH);
@@ -13,3 +13,19 @@ function appendSpan(selector) {
 
 appendSpan('.horizontal');
 appendSpan('.vertical');
+
+let animation = new TimelineMax({repeat: -1});
+let $horizontalSpan = '.container .horizontal span';
+let $verticalSpan = '.container .vertical span';
+
+animation.to($horizontalSpan, 1, {rotation: 45}, 'step1')
+    .to($horizontalSpan, 1, {x: '-10px', y: '-10px'}, 'step2')
+    .to($horizontalSpan, 1, {rotation: 0, x: '0', y: '0', scaleX: 0.5, scaleY: 2}, 'step3')
+    .to($horizontalSpan, 1, {rotation: 90, scaleX: 1, scaleY: 1}, 'step4')
+    
+animation.to($verticalSpan, 1, {rotation: 45}, 'step1')
+    .to($verticalSpan, 1, {x: '10px', y: '10px'}, 'step2')
+    .to($verticalSpan, 1, {x: '0', y: '0', scaleX: 2, scaleY: 0.5}, 'step3')
+    .to($verticalSpan, 1, {rotation: 90, scaleX: 1, scaleY: 1}, 'step4')
+
+animation.timeScale(2);
