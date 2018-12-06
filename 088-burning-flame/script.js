81d2201b3aca3446e9ae199fc8d4c9bb3c331b7a
comehope <comehope@163.com>
2018-12-06 15:16:55 +0800

    use dom api instead of d3.js

--- 088-burning-flame/script.js
+++ 088-burning-flame/script.js
@@ -1,10 +1,18 @@
 const COUNT_OF_PARTICLES = 100;
+const $ = selector => document.querySelectorAll(selector)
+const dom = {
+    flame: $('.flame')[0]
+}
 
-d3.select('.flame')
-    .style('--particles', COUNT_OF_PARTICLES)
-    .selectAll('span')
-    .data(d3.range(COUNT_OF_PARTICLES))
-    .enter()
-    .append('span')
-    .style('--n', (d) => d + 1)
-    .style('--rnd', () => Math.random());
+let particles = new Array(COUNT_OF_PARTICLES)
+    .fill('')
+    .map(x => Math.random())
+
+dom.flame.style.setProperty('--particles', COUNT_OF_PARTICLES)
+
+particles.forEach((particle, i) => {
+    let span = document.createElement('span')
+    span.style.setProperty('--n', i + 1)
+    span.style.setProperty('--rnd', particle)
+    dom.flame.appendChild(span)
+})
