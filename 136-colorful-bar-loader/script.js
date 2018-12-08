de8dc154e55c8b88ed578fc427a996534ac72c9f
comehope <comehope@163.com>
2018-12-08 15:54:08 +0800

    use dom api instead of d3.js

--- 136-colorful-bar-loader/script.js
+++ 136-colorful-bar-loader/script.js
@@ -1,14 +1,14 @@
 const HUE_DEG = 12;
-const COUNT = 360 / HUE_DEG;
+const COUNT_OF_LINES = 360 / HUE_DEG;
 
-d3.select('.loader')
-    .selectAll('span')
-    .data(d3.range(COUNT).map(d => d + 1))
-    .enter()
-    .append('span')
-    .style('background-color', (d) => d % 2 === 1
-        ? `hsl(${d * HUE_DEG}, 100%, 65%)`
-        : 'black');
+let lines = new Array(COUNT_OF_LINES).fill('')
+lines.forEach((x, i) => {
+    let span = document.createElement('span')
+    span.style.backgroundColor = ((i + 1) % 2) === 1
+        ? `hsl(${(i + 1) * HUE_DEG}, 100%, 65%)`
+        : 'black'
+    document.querySelector('.loader').appendChild(span)
+})
 
 let animation = new TimelineMax({repeat: -1});
 animation.staggerFrom('.loader span', 0.5, {scaleX: 0}, 0.4);
