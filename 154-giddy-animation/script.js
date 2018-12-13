9ed27e78691eee0c3b80af8613788cce61fa0194
comehope <comehope@163.com>
2018-12-13 22:40:44 +0800

    use dom api instead of d3.js

--- 154-giddy-animation/script.js
+++ 154-giddy-animation/script.js
@@ -1,9 +1,12 @@
-const COLUMNS = 15;
+const COLUMNS = 15
+const MAX_DELAY_SECONDS = 4
+const dom = {
+    container: document.querySelector('.container')
+}
 
-d3.select('.container')
-    .style('--columns', COLUMNS)
-    .selectAll('span')
-    .data(d3.range(COLUMNS * COLUMNS))
-    .enter()
-    .append('span')
-    .style('--delay', () => Math.random() * 4);
+dom.container.style.setProperty('--columns', COLUMNS)
+Array(COLUMNS * COLUMNS).fill('').forEach(() => {
+    let span = document.createElement('span')
+    span.style.setProperty('--delay', Math.random() * MAX_DELAY_SECONDS)
+    dom.container.appendChild(span)
+})
