d8cd20452126624da235b6290d988bbca7c324e4
comehope <comehope@163.com>
2018-12-16 10:26:58 +0800

    use dom api instead of d3.js

--- 108-black-and-white-overlap-animation/script.js
+++ 108-black-and-white-overlap-animation/script.js
@@ -1,9 +1,9 @@
-const COUNT_OF_PARTICLES = 30;
+const COUNT_OF_PARTICLES = 30
+let container = document.querySelector('.circles')
 
-d3.select('.circles')
-    .style('--particles', COUNT_OF_PARTICLES)
-    .selectAll('span')
-    .data(d3.range(COUNT_OF_PARTICLES))
-    .enter()
-    .append('span')
-    .style('--n', (d) => d + 1);
+container.style.setProperty('--particles', COUNT_OF_PARTICLES)
+Array(COUNT_OF_PARTICLES).fill('').forEach((x, i) => {
+    let span = document.createElement('span')
+    span.style.setProperty('--n', i + 1)
+    container.appendChild(span)
+})
