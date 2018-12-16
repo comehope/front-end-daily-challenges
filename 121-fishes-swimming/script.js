f3e5abbc207cf0695890a2aa72a71aa71d981198
comehope <comehope@163.com>
2018-12-16 11:02:32 +0800

    use dom api instead of d3.js

--- 121-fishes-swimming/script.js
+++ 121-fishes-swimming/script.js
@@ -1,26 +1,38 @@
+const INITIAL_FISHS = 3
+let random = (min, max) => Math.random() * (max - min) + min
+let container = document.querySelector('body')
+
 function buildFish(e) {
-    let fish = d3.select('body')
-        .append('div')
-        .attr('class', 'fish')
-        .style('--size', d3.randomUniform(5, 8)())
-        .style('--color', d3.randomUniform(-60, 15)())
-        .style('--duration', d3.randomUniform(3, 6)())
-        .style('--top', e.clientY ? e.clientY : d3.randomUniform(100, window.innerHeight)());
-    
-    fish.append('span').attr('class', 'body');
-    fish.append('span').attr('class', 'eye');
-    fish.append('span').attr('class', 'fin');
-    fish.append('span').attr('class', 'tail');
+    let fish = document.createElement('div')
+
+    fish.className = 'fish'
+    fish.style.setProperty('--size', random(5, 8))
+    fish.style.setProperty('--color', random(-60, 15))
+    fish.style.setProperty('--duration', random(3, 6))
+    fish.style.setProperty('--top', e.clientY ? e.clientY : random(100, window.innerHeight))
+    fish.innerHTML = [
+        '<span class="body"></span>',
+        '<span class="eye"></span>',
+        '<span class="fin"></span>',
+        '<span class="tail"></span>'].join('')
+
+    container.appendChild(fish)
 }
 
 function buildRipple(e) {
-    d3.select('body')
-        .append('div')
-        .attr('class', 'ripple')
-        .style('--left', e.clientX)
-        .style('--top', e.clientY);
+    let ripple = document.createElement('div')
+
+    ripple.className = 'ripple'
+    ripple.style.setProperty('--left', e.clientX)
+    ripple.style.setProperty('--top', e.clientY)
+
+    container.append(ripple)
 };
 
-window.addEventListener('click', buildFish);
-window.addEventListener("click", buildRipple);
-d3.range(3).forEach(buildFish);
+function init() {
+    window.addEventListener('click', buildFish);
+    window.addEventListener("click", buildRipple);
+    Array(INITIAL_FISHS).fill('').forEach(buildFish)
+}
+
+window.onload = init
