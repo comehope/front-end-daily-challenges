dd74bd57622e206700d3cc507c9c53d12e95793e
comehope <comehope@163.com>
2018-12-17 11:17:36 +0800

    use dom api instead of d3.js

--- 139-glowing-particles-animation/script.js
+++ 139-glowing-particles-animation/script.js
@@ -1,12 +1,15 @@
-const COUNT = 200;
+const COUNT = 200
+let random = (min, max) => Math.random() * (max - min) + min
+let container = document.querySelector('.container')
 
-d3.select('.container')
-    .selectAll('span')
-    .data(d3.range(COUNT))
-    .enter()
-    .append('span')
-    .style('--x', () => d3.randomUniform(1, 99)())
-    .style('--y', () => d3.randomUniform(1, 99)())
-    .style('--n', d => d)
-    .style('--dark-color', (d) => d3.color(`hsl(${70 + d * 0.1}, 100%, 50%)`))
-    .style('--bright-color', (d) => d3.color(`hsl(${70 + d * 0.1}, 100%, 50%)`).brighter(0.15));
+Array(COUNT).fill('').forEach((x, i) => {
+    let span = document.createElement('span')
+
+    span.style.setProperty('--x', random(1, 99))
+    span.style.setProperty('--y', random(1, 99))
+    span.style.setProperty('--n', i)
+    span.style.setProperty('--dark-color', `hsl(${70 + i * 0.1}, 100%, 50%)`)
+    span.style.setProperty('--bright-color', `hsl(${85 + i * 0.1}, 100%, 60%)`)
+
+    container.appendChild(span)
+})
