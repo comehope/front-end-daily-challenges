c5c5d8130fc23e1a40e6f6f45d284d16a318f26c
comehope <comehope@163.com>
2018-12-17 12:00:56 +0800

    remove javascript codes, replaced by pure css

--- 095-rotating-worm/script.js
+++
@@ -1,9 +0,0 @@
-const COUNT_OF_PARTICLES = 12
-let container = document.querySelector('.worm')
-
-container.style.setProperty('--particles', COUNT_OF_PARTICLES)
-Array(COUNT_OF_PARTICLES).fill('').forEach((x, i) => {
-    let span = document.createElement('span')
-    span.style.setProperty('--n', i + 1)
-    container.appendChild(span)
-})
