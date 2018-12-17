2a0e09110f170cfa9dea2f80690b1e3faab055e5
comehope <comehope@163.com>
2018-12-17 13:39:43 +0800

    remove javascript codes, replaced by pure css

--- 101-pendulums/script.js
+++
@@ -1,9 +0,0 @@
-const COUNT_OF_PARTICLES = 15;
-
-let particles = new Array(COUNT_OF_PARTICLES).fill('')
-
-particles.forEach((x, i) => {
-    let span = document.createElement('span')
-    span.style.setProperty('--n', i + 1)
-    document.querySelector('.pendulums').append(span)
-})
