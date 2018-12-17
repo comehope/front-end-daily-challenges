d1256b4897a45feeaab56845d2abb54c72bde483
comehope <comehope@163.com>
2018-12-17 11:54:31 +0800

    improve readability

--- 088-burning-flame/script.js
+++ 088-burning-flame/script.js
@@ -1,18 +1,11 @@
-const COUNT_OF_PARTICLES = 100;
-const $ = selector => document.querySelectorAll(selector)
-const dom = {
-    flame: $('.flame')[0]
-}
+const COUNT_OF_PARTICLES = 100
+const container = document.querySelector('.flame')
 
-let particles = new Array(COUNT_OF_PARTICLES)
-    .fill('')
-    .map(x => Math.random())
+container.style.setProperty('--particles', COUNT_OF_PARTICLES)
 
-dom.flame.style.setProperty('--particles', COUNT_OF_PARTICLES)
-
-particles.forEach((particle, i) => {
+Array(COUNT_OF_PARTICLES).fill('').forEach((particle, i) => {
     let span = document.createElement('span')
     span.style.setProperty('--n', i + 1)
-    span.style.setProperty('--rnd', particle)
-    dom.flame.appendChild(span)
+    span.style.setProperty('--rnd', Math.random())
+    container.appendChild(span)
 })
