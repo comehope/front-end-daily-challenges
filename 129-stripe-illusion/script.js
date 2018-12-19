f8821c00f4eb2dfebb21abb6a170c94f23f252b5
comehope <comehope@163.com>
2018-12-19 17:58:57 +0800

    remove toggle button

--- 129-stripe-illusion/script.js
+++
@@ -1,6 +0,0 @@
-let $toggle = document.getElementsByClassName('toggle')[0]
-let $tracks = Array.from(document.getElementsByClassName('track'))
-
-$toggle.addEventListener('click', function() {
-    $tracks.forEach(track => track.classList.toggle('highlights'))
-})
