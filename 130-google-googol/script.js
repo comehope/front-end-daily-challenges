bb26dac9868e465e13bcb8db9d46fe0573aaa053
comehope <comehope@163.com>
2018-12-21 11:37:33 +0800

    use dom api instead of d3.js

--- 130-google-googol/script.js
+++ 130-google-googol/script.js
@@ -1,12 +1,11 @@
 function getColor(excludedColor) {
     let colors = new Set(['blue', 'red', 'yellow', 'green'])
     colors.delete(excludedColor)
-    return Array.from(colors)[Math.floor(d3.randomUniform(0, colors.size)())]
+    return Array.from(colors)[Math.floor(Math.random() * colors.size)]
 }
 
-const ZEROS = d3.range(100).map(()=>0)
-const ONE = {number: 1, color: 'blue'}
-let numbers = ZEROS.reduce(function (numberObjects, d) {
+let ONE = {number: 1, color: 'blue'}
+let zeros = Array(100).fill(0).reduce(function (numberObjects, d) {
     numberObjects.push({
         number: d,
         color: getColor(numberObjects[numberObjects.length - 1].color)
@@ -14,10 +13,10 @@ let numbers = ZEROS.reduce(function (numberObjects, d) {
     return numberObjects
 }, [ONE])
 
-d3.select('.zeros')
-    .selectAll('span')
-    .data(numbers)
-    .enter()
-    .append('span')
-    .style('--c', d => `var(--${d.color})`)
-    .text(d => d.number)
+let container = document.querySelector('.zeros')
+zeros.forEach((d) => {
+    let span = document.createElement('span')
+    span.style.setProperty('--c', `var(--${d.color})`)
+    span.innerText = d.number
+    container.appendChild(span)
+})
